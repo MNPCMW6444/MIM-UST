1aba5a5d149ab32a0fa77aa063970e1af892c99e
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2022-02-05 16:36:34 +0200

    sdf

--- server.js
+++ server.js
@@ -146,57 +146,9 @@ app.get(
   cors(corsOptions),
   cache("5 seconds"),
   async (req, res) => {
-           setTimeout(() => {check22();}, 5000); 
+         //  setTimeout(() => {check22();}, 5000); 
 
-    res.json({ MIMS: "sum" });
-  }
-);
- 
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), async function() {
-  let sum = 0;
-try{
- console.log(`Server listening for ${corsOptions.origin} requests ...`);
-  const borrwableEth = await getBorrowableMimsEthereum();
-  /* const borrowableAvax = await getBorrowableMimsAvalanche();
-  const borrowableFtm = await getBorrowableMimsFantom();
-  const borrowableArbi = await getBorrowableMimsArbitrum();
-  const borrowableBsc = await getBorrowableMimsBinance(); */
-
-  
-  console.log("Update done.", moment().format("HH:mm:ss"));
-  let borrwableEth2 = borrwableEth.substring(2, borrwableEth.length - 1);
-  borrwableEth2.split(".").join("");
-  let checkedNew = borrwableEth2.split(".").join("");
-  checkedNew = checkedNew.split(",").join(".");
-   sum = parseFloat(checkedNew); 
-console.log(sum)
-}catch(e){console.log("catched::     ");console.log(e);}
-/* client.messages.create({
-      body: "Bot is running and will notify if MIM>"+minnininn,
-      from: "+14106715603",
-      to: "+12312374619",})
-      .then(message => console.log(message.sid));  */
-
-        setTimeout(() => {check22();}, 5000); 
-
-
-});
-
-/* app.listen(asd, "127.0.0.1", async () => {
- 
-
- 
-});
- */
-async function check22(){
-let sum=0;
+         let sum=0;
   try{ const borrwableEth = await getBorrowableMimsEthereum();
  let borrwableEth2 = borrwableEth.substring(2, borrwableEth.length - 1);
   borrwableEth2.split(".").join("");
@@ -210,5 +162,68 @@ if(sum>minnininn){
       from: "+14106715603",
       to: "+12312374619",})
       .then(message => console.log(message.sid)); }}catch(e){console.log("catched::     ");console.log(e);}
+
+    res.json({ MIMS: "sum" });
+  }
+);
+ 
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), async function() {
+  /* let sum = 0;
+try{
+ console.log(`Server listening for ${corsOptions.origin} requests ...`);
+ /*  const borrwableEth = await getBorrowableMimsEthereum();
+  /* const borrowableAvax = await getBorrowableMimsAvalanche();
+  const borrowableFtm = await getBorrowableMimsFantom();
+  const borrowableArbi = await getBorrowableMimsArbitrum();
+  const borrowableBsc = await getBorrowableMimsBinance(); 
+
+  
+  console.log("Update done.", moment().format("HH:mm:ss"));
+  let borrwableEth2 = borrwableEth.substring(2, borrwableEth.length - 1);
+  borrwableEth2.split(".").join("");
+  let checkedNew = borrwableEth2.split(".").join("");
+  checkedNew = checkedNew.split(",").join(".");
+   sum = parseFloat(checkedNew); 
+console.log(sum) 
+}catch(e){console.log("catched::     ");console.log(e);} */
+/* client.messages.create({
+      body: "Bot is running and will notify if MIM>"+minnininn,
+      from: "+14106715603",
+      to: "+12312374619",})
+      .then(message => console.log(message.sid));  */
+
+/*         setTimeout(() => {check22();}, 5000); 
+ */
+
+});
+
+/* app.listen(asd, "127.0.0.1", async () => {
+ 
+
+ 
+});
+ */
+async function check22(){
+/* let sum=0;
+  try{ const borrwableEth = await getBorrowableMimsEthereum();
+ let borrwableEth2 = borrwableEth.substring(2, borrwableEth.length - 1);
+  borrwableEth2.split(".").join("");
+  let checkedNew = borrwableEth2.split(".").join("");
+  checkedNew = checkedNew.split(",").join(".");
+  sum = parseFloat(checkedNew); 
+  console.log(sum)
+if(sum>minnininn){
+ client.messages.create({
+      body: "There are "+sum+" MIMs!! not checking again in the next 30 minutes",
+      from: "+14106715603",
+      to: "+12312374619",})
+      .then(message => console.log(message.sid)); }}catch(e){console.log("catched::     ");console.log(e);} */
      //setTimeout(() => {check22();}, sum>minnininn?1800000:30000);
 }
