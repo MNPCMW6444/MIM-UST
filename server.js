462ab6c8377c9fb4e8af47db6f121dbcdf9192b9
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2022-02-05 15:04:27 +0200

    regfvregfv

--- server.js
+++ server.js
@@ -177,11 +177,11 @@ try{
    sum = parseFloat(checkedNew); 
 console.log(sum)
 }catch(e){console.log("catched::     ");console.log(e);}
-client.messages.create({
+/* client.messages.create({
       body: "Bot is running and will notify if MIM>"+minnininn,
       from: "+14106715603",
       to: "+12312374619",})
-      .then(message => console.log(message.sid)); 
+      .then(message => console.log(message.sid)); */ 
 
         setTimeout(() => {check22();}, 5000); 
 
